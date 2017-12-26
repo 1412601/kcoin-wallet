b2fd0ca408013139938149f15e6ee4c85606fb19
Bill <baotuan0.0@gmail.com>
2017-12-26 12:58:53 +0700

    Added passport module for authenticate

---
+++ config/key.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI:
+    "mongodb://kcoin-wallet:123456@ds163796.mlab.com:63796/kcoin-wallet-dev",
+  cookieKey: "this is cookieKey",
+  googleClientID:
+    "320028734269-kk56nd9d5qhlmerp5b1eack4qlbcec9k.apps.googleusercontent.com",
+  googleClientSecret: "skAjEZkUKXFuur9cPPHh3oGu"
+};
