1f6f4ad56b76c7f995f50f9d6bf078db42e0b655
Bill <baotuan0.0@gmail.com>
2017-12-28 15:51:26 +0700

    Added two-factor authenticate

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,17 @@
 const passport = require("passport");
+const twoFactor = require("node-2fa");
 
 module.exports = app => {
+  app.get("/", (req, res) => {
+    const newSecret = twoFactor.generateSecret({
+      name: "Kcoin-Wallet",
+      account: "johndoe"
+    });
+    const token = twoFactor.generateToken("T6ZYUIHLPWFFVTUZ3Z2OD63RKZ7XCY3J");
+    console.log("TOKEN", token);
+    res.send(newSecret);
+  });
+
   app.get(
     "/auth/google",
     passport.authenticate("google", {
