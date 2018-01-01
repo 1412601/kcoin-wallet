4e988929a2af3e47f010cdcfe6546e187366dedb
Bill <baotuan0.0@gmail.com>
2018-01-01 20:27:44 +0700

    [Client] Added Landing Page  + General info page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 import { connect } from "react-redux";
 import * as action from "../actions";
 
 import Home from "./Home";
-import Header from "./Header";
+import HeaderApp from "./Header";
+
+const LandingPage = () => (
+  <div style={{ textAlign: "center" }}>
+    <Header as="h2" icon textAlign="center">
+      <Header.Content>Welcome to Kcoin wallet</Header.Content>
+    </Header>
+    <div>Login to continue</div>
+  </div>
+);
+
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
 
   render() {
+    const { auth } = this.props;
     return (
       <BrowserRouter>
         <Container>
-          <Header />
-          <Route exact path="/" component={Home} />
+          <HeaderApp />
+          <Route
+            exact
+            path="/"
+            component={auth === null ? LandingPage : Home}
+          />
         </Container>
       </BrowserRouter>
     );
   }
 }
 
-export default connect(() => ({}), action)(App);
+export default connect(({ authReducer }) => ({ auth: authReducer }), action)(
+  App
+);
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,136 @@
 import React, { Component } from "react";
-
+import {
+  Grid,
+  Form,
+  Header,
+  Container,
+  Button,
+  Segment,
+  Popup,
+  Label,
+  Menu
+} from "semantic-ui-react";
+import QRCode from "qrcode.react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 class Home extends Component {
-  state = {};
+  state = { showQR: false };
+  componentDidMount() {
+    this.props.getWallet();
+  }
+
+  renderMenu() {
+    return (
+      <Menu fluid vertical>
+        <Menu.Item>
+          <Menu.Header>Your account</Menu.Header>
+          <Menu.Menu>
+            <Menu.Item
+              name="General information"
+              // active={activeItem === "Profile"}
+              // onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="Transactions"
+              // active={activeItem === "Profile"}
+              // onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="Menu"
+              // active={activeItem === "Profile"}
+              // onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+
   render() {
-    return <h2>Home page</h2>;
+    const user = this.props.user || {
+      balance: 0,
+      availableBalance: 0,
+      address: ""
+    };
+    const { showQR } = this.state;
+    return (
+      <Container padded="very">
+        <Grid>
+          <Grid.Column width={4}>{this.renderMenu()}</Grid.Column>
+          <Grid.Column width={12}>
+            <Segment padded="very" style={{ height: "80vh" }}>
+              <Form>
+                <Form.Group>
+                  <Form.Field
+                    width={5}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "flex-start"
+                    }}
+                  >
+                    <Header size="medium" textAlign="center">
+                      Balance
+                    </Header>
+                  </Form.Field>
+                  <Form.Field width={11}>
+                    <Popup
+                      trigger={
+                        <Label as="a" color="green" size="big">
+                          {user.balance} KCoin
+                        </Label>
+                      }
+                      content="Number of Kcoin that currently own"
+                      position="right center"
+                    />
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Field
+                    width={5}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "flex-start"
+                    }}
+                  >
+                    <Header size="medium" textAlign="center">
+                      Available balance
+                    </Header>
+                  </Form.Field>
+                  <Form.Field width={11}>
+                    <Popup
+                      trigger={
+                        <Label as="a" color="yellow" size="big">
+                          {user.availableBalance} KCoin
+                        </Label>
+                      }
+                      content="Number of Kcoin that available to use"
+                      position="right center"
+                    />
+                  </Form.Field>
+                </Form.Group>
+                <Header size="medium">Address [{user.address}]</Header>
+                <Button
+                  primary
+                  onClick={() => this.setState({ showQR: !showQR })}
+                >
+                  {showQR ? "Hide" : "Show"} QRcode
+                </Button>
+                {this.state.showQR ? (
+                  <div style={{ textAlign: "center", margin: 10 }}>
+                    <QRCode value={user.address} size={350} />
+                  </div>
+                ) : null}
+              </Form>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    );
   }
 }
 
-export default Home;
+export default connect(({ userReducer }) => ({ user: userReducer }), actions)(
+  Home
+);
