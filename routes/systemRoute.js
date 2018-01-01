8a7a6cc6d9940125904ce06dabf6af302057a6bc
Văn Thé Tùng <thetung.van@gmail.com>
2018-01-01 20:34:54 +0700

    Added system statistics route

--- routes/systemRoute.js
+++ routes/systemRoute.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const Transaction = mongoose.model("transactions");
+const Admin = mongoose.model("admin");
 const Wallet = mongoose.model("wallets");
 
 module.exports = app => {
@@ -24,5 +25,12 @@ module.exports = app => {
       "balance"
     );
     res.send({ allAddress });
+  });
+
+  app.get("/system/statistics", async (req, res) => {
+    const totalUser = await User.count({});
+    const totalTransaction = await Transaction.count({});
+    const { balance: systemBalance } = await Admin.findOne({}, "balance");
+    res.send({ totalUser, totalTransaction, systemBalance });
   });
 };
