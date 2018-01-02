e6f84e7800000da4215dd4356bd689936e3f7b93
Bill <baotuan0.0@gmail.com>
2018-01-02 11:17:11 +0700

    [Server] GET user's transaction base on type

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -2,11 +2,33 @@ const mongoose = require("mongoose");
 const Transaction = mongoose.model("transactions");
 
 module.exports = app => {
-  app.get("/user/transaction", async (req, res) => {
-    const transactions = await Transaction.find({
-      $or: [{ from: req.user.id }, { to: req.user.id }]
-    });
-    res.send({ transactions });
+  app.get("/api/user/transaction", async (req, res) => {
+    const { type } = req.query;
+    let transactions;
+    switch (type) {
+      case "send":
+        transactions = await Transaction.find({
+          from: req.user.id,
+          status: 2
+        });
+      case "receive":
+        transactions = await Transaction.find({
+          to: req.user.id,
+          status: 2
+        });
+      case "init":
+        transactions = await Transaction.find({
+          from: req.user.id,
+          status: 0
+        });
+      case "pending":
+        transactions = await Transaction.find({
+          from: req.user.id,
+          status: 1
+        });
+    }
+
+    res.send(transactions);
   });
 
   app.delete("/transaction/:id", async (req, res) => {
