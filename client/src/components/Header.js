f0b555eaab51d3e02230c741326a53746a5eb24f
Bill <baotuan0.0@gmail.com>
2018-01-02 21:55:44 +0700

    [Deploy] Added node engine

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,12 +16,9 @@ class Header extends Component {
   renderContent() {
     return this.props.auth === null ? (
       <Menu.Item>
-        {/* <Button color="google plus" as="a" href="/auth/google">
+        <Button color="google plus" as="a" href="/auth/google">
           <Icon name="google plus" /> Login with Google
-        </Button> */}
-        <a className="ui google plus button" href="/auth/google">
-          Login with google
-        </a>
+        </Button>
       </Menu.Item>
     ) : (
       [
