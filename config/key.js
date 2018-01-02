4fdbf13a3a683e3ba49ade2171660bba7b22c061
Bill <baotuan0.0@gmail.com>
2018-01-02 18:28:42 +0700

    [Deploy] Config key for production

--- config/key.js
+++ config/key.js
@@ -1,17 +1,5 @@
-dev = {
-  mongoURI:
-    "mongodb://kcoin-wallet:123456@ds163796.mlab.com:63796/kcoin-wallet-dev",
-  cookieKey: "this is cookieKey",
-  googleClientID:
-    "320028734269-kk56nd9d5qhlmerp5b1eack4qlbcec9k.apps.googleusercontent.com",
-  googleClientSecret: "skAjEZkUKXFuur9cPPHh3oGu",
-  sendGridKey:
-    "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",
-  redirectDomain: "Temp"
-};
-
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
 } else {
-  module.exports = dev;
+  module.exports = require("./dev");
 }
