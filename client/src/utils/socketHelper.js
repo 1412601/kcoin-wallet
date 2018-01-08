c61d5fe14c1d493c2a79f06b726c866fe8143653
Bill <baotuan0.0@gmail.com>
2018-01-08 17:31:40 +0700

    [Client] config port socket io in production env

--- client/src/utils/socketHelper.js
+++ client/src/utils/socketHelper.js
@@ -1,4 +1,7 @@
 import socketIOClient from "socket.io-client";
-const endpoint = "http://localhost:5000";
+const endpoint =
+  process.env.NODE_ENV === "production"
+    ? "https://kcoin-wallet.herokuapp.com"
+    : "http://localhost:5000";
 
 export default socketIOClient(endpoint);
